f39292a2c0f56a863e13c59a6f49261d7423dfe1
trzaskom <m_trzaskowski@wp.pl>
2019-01-16 19:29:43 +0100

    Remove redundant tabs

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -7,8 +7,6 @@ import {FormsModule} from '@angular/forms';
 import {TabsPageRoutingModule} from './tabs.router.module';
 
 import {TabsPage} from './tabs.page';
-import {ContactPageModule} from '../contact/contact.module';
-import {AboutPageModule} from '../about/about.module';
 import {HomePageModule} from '../home/home.module';
 import {CourtsPageModule} from '../courts/courts.module';
 import {MatchesPageModule} from '../matches/matches.module';
--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -3,8 +3,6 @@ import {RouterModule, Routes} from '@angular/router';
 
 import {TabsPage} from './tabs.page';
 import {HomePage} from '../home/home.page';
-import {AboutPage} from '../about/about.page';
-import {ContactPage} from '../contact/contact.page';
 import {CourtsPage} from '../courts/courts.page';
 import {MatchesPage} from '../matches/matches.page';
 import {WeatherPage} from '../weather/weather.page';
