a9b040a922dda5abb501a4245edbd092fcbae48d
trzaskom <m_trzaskowski@wp.pl>
2019-01-16 20:08:16 +0100

    Add routes to new tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AuthGuard} from '../client/auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'courts', loadChildren: './courts/courts.module#CourtsPageModule' },
-  { path: 'matches', loadChildren: './matches/matches.module#MatchesPageModule' },
-  { path: 'weather', loadChildren: './weather/weather.module#WeatherPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+    {path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [AuthGuard]},
+    {path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthGuard]},
+    {path: 'courts', loadChildren: './courts/courts.module#CourtsPageModule', canActivate: [AuthGuard]},
+    {path: 'matches', loadChildren: './matches/matches.module#MatchesPageModule', canActivate: [AuthGuard]},
+    {path: 'weather', loadChildren: './weather/weather.module#WeatherPageModule', canActivate: [AuthGuard]},
+    {path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule', canActivate: [AuthGuard]},
+    {path: 'signup', loadChildren: './signup/signup.module#SignupPageModule'},
+    {path: 'login', loadChildren: './login/login.module#LoginPageModule'}
 ];
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
