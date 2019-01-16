216f34e7fdc8f0ec05c5bd09a5f11223827aae78
trzaskom <m_trzaskowski@wp.pl>
2019-01-16 20:07:35 +0100

    Add JWTModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,24 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {GoogleMaps} from '@ionic-native/google-maps';
+import {environment} from '../environments/environment';
+import {JwtModule} from '@auth0/angular-jwt';
+
+export function tokenGetter() {
+    return localStorage.getItem('jwt_token');
+}
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
-    imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
+    imports: [BrowserModule, HttpClientModule,
+        JwtModule.forRoot({
+            config: {
+                tokenGetter: tokenGetter,
+                whitelistedDomains: environment.whitelistedDomains
+            }
+        }),
+        IonicModule.forRoot(), AppRoutingModule],
     providers: [
         StatusBar,
         SplashScreen,
