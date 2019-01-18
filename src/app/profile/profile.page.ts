e3f3e1e28f584472e9cce1257bf6c0d164453777
trzaskom <m_trzaskowski@wp.pl>
2019-01-18 18:29:24 +0100

    Refactor page to display users details

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,22 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 import {RestService} from '../../client/api/rest.service';
+import {FormControl, FormGroup} from '@angular/forms';
+import {NavController} from '@ionic/angular';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../../client/auth/auth.service';
+import {environment} from '../../environments/environment';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.page.html',
     styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage {
 
-/*    private users: Array<any>;
+    loggedUser;
 
-    constructor(public restService: RestService) {
-    }*/
-
-    ngOnInit() {
-/*        this.restService.getUsers().subscribe(users => {
-            this.users = users;
-        });*/
+    constructor(private readonly navCtrl: NavController, public authService: AuthService) {
+        this.getUserDetails();
     }
+
+    getUserDetails() {
+        this.authService.getCurrentUser().subscribe(() => {
+            this.loggedUser = this.authService.loggedUser;
+        });
+    }
+
+    updateRating() {
+        this.navCtrl.navigateRoot(['rating'], true, {replaceUrl: true});
+    }
+
 
 }
