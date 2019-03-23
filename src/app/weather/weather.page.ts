99064a8a61bdff5bc3fbfd836970ce2138cbccac
trzaskom <m_trzaskowski@wp.pl>
2019-03-23 15:20:15 +0100

    Remove test code for acquiring user

--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-/*import {RestService} from '../../client/api/rest.service';
-import {User} from '../../client/model/User';*/
 
 @Component({
     selector: 'app-weather',
@@ -8,23 +6,6 @@ import {User} from '../../client/model/User';*/
     styleUrls: ['./weather.page.scss'],
 })
 export class WeatherPage implements OnInit {
-
-    /*newUser: any;
-    user: any = {
-        name: 'Tomasz',
-        surname: 'Tomaszewski',
-        email: 'tom@tom.wp.pl',
-        gender: 'male',
-        age: 23
-    };
-
-    constructor(public restService: RestService) {
-        console.log(this.user);
-        restService.createTestUser(this.user).subscribe(newUser => {
-            this.newUser = newUser;
-        });
-        console.log(this.newUser);
-    }*/
 
     ngOnInit() {
     }
