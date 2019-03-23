74ccecdc300f0bc87ee229baeb2d1b1c989f3663
trzaskom <m_trzaskowski@wp.pl>
2019-03-23 15:18:31 +0100

    Update import path

--- src/app/rating/rating.page.ts
+++ src/app/rating/rating.page.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NavController} from '@ionic/angular';
 import {HttpClient} from '@angular/common/http';
-import {AuthService} from '../../client/auth/auth.service';
+import {AuthService} from '../auth/auth.service';
 import {environment} from '../../environments/environment';
 
 @Component({
-  selector: 'app-rating',
-  templateUrl: './rating.page.html',
-  styleUrls: ['./rating.page.scss'],
+    selector: 'app-rating',
+    templateUrl: './rating.page.html',
+    styleUrls: ['./rating.page.scss'],
 })
 export class RatingPage {
 
@@ -24,7 +24,8 @@ export class RatingPage {
     }
 
     updateRating() {
-        this.rating = (+this.forehand + +this.backhand + +this.movement + +this.firstServe + +this.secondServe + +this.returnOfServe + +this.volleys) / 7;
+        this.rating = (+this.forehand + +this.backhand + +this.movement + +this.firstServe + +this.secondServe
+            + +this.returnOfServe + +this.volleys) / 7;
         console.log(this.rating);
         this.http.put(`${environment.serverURL}/rating`, this.rating).subscribe();
         this.navCtrl.navigateRoot([''], true, {replaceUrl: true});
