36ae7a22f1fc8a3f828fb38a2f436b3927e08f35
trzaskom <m_trzaskowski@wp.pl>
2019-03-23 15:09:10 +0100

    Add map and player card to tab for presentation purposes

--- src/app/courts/courts.page.ts
+++ src/app/courts/courts.page.ts
@@ -31,40 +31,59 @@ export class CourtsPage implements AfterViewInit {
         navigator.geolocation.getCurrentPosition((location) => {
             console.log(location);
             map = new google.maps.Map(this.mapElement.nativeElement, {
-                center: {lat: location.coords.latitude, lng: location.coords.longitude},
-                zoom: 15
+                center: {lat: 52.2193803, lng: 21.0049868},
+                zoom: 15,
             });
+            this.addMarker(map);
         });
     }
-/*            infoWindow = new google.maps.InfoWindow();
-            const service = new google.maps.places.PlacesService(map);
-            service.nearbySearch({
-                location: {lat: location.coords.latitude, lng: location.coords.longitude},
-                radius: 1000,
-                type: ['store']
-            }, (results, status) => {
-                if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    for (let i = 0; i < results.length; i++) {
-                        this.createMarker(results[i]);
-                    }
-                }
-            });
-        }, (error) => {
-            console.log(error);
-        }, options);
-        const myplace = {lat: -33.8665, lng: 151.1956};
-    }
 
-    createMarker(place) {
-        const placeLoc = place.geometry.location;
+    addMarker(mapa: any) {
+
+        const image = '/bluedot.png';
         const marker = new google.maps.Marker({
-            map: map,
-            position: placeLoc
+            map: mapa,
+            animation: google.maps.Animation.DROP,
+            position: {lat: 52.2193803, lng: 21.0049868},
+            content: 'YOU ARE HERE'
+        });
+        const marker2 = new google.maps.Marker({
+            map: mapa,
+            animation: google.maps.Animation.DROP,
+            position: {lat: 52.217356, lng: 21.010136}
         });
 
-        google.maps.event.addListener(marker, 'click', function () {
-            infoWindow.setContent(place.name);
-            infoWindow.open(map, this);
-        });
-    }*/
+    }
+
+    /*            infoWindow = new google.maps.InfoWindow();
+                const service = new google.maps.places.PlacesService(map);
+                service.nearbySearch({
+                    location: {lat: location.coords.latitude, lng: location.coords.longitude},
+                    radius: 1000,
+                    type: ['store']
+                }, (results, status) => {
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        for (let i = 0; i < results.length; i++) {
+                            this.createMarker(results[i]);
+                        }
+                    }
+                });
+            }, (error) => {
+                console.log(error);
+            }, options);
+            const myplace = {lat: -33.8665, lng: 151.1956};
+        }
+
+        createMarker(place) {
+            const placeLoc = place.geometry.location;
+            const marker = new google.maps.Marker({
+                map: map,
+                position: placeLoc
+            });
+
+            google.maps.event.addListener(marker, 'click', function () {
+                infoWindow.setContent(place.name);
+                infoWindow.open(map, this);
+            });
+        }*/
 }
