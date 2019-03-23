09e3ab2c4145136823128e945b67b23e5640ee4a
trzaskom <m_trzaskowski@wp.pl>
2019-03-23 15:26:59 +0100

    Add logic for searching players

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,35 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthService} from '../../client/auth/auth.service';
+import {AuthService} from '../auth/auth.service';
 import {JwtHelperService} from '@auth0/angular-jwt';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs/Rx';
+import {GeolocationService} from '../../client/api/geolocation/geolocation.service';
+import {SearchService} from '../../client/api/search/search.service';
+import {UserSearchDTO} from '../../client/model/UserSearchDTO';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss']
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
+    searchListView = false;
+    searchDetails = false;
+    detailsSwitch = 'details';
+    detailedUser: UserSearchDTO;
     user: string;
     message: string;
+    searchRating: any;
+    initialMin = 10;
+    initialMax = 80;
+    minRating: any;
+    maxRating: any;
+    maxRange: any;
 
     constructor(private readonly authService: AuthService,
-                jwtHelper: JwtHelperService,
-                private readonly httpClient: HttpClient) {
+                jwtHelper: JwtHelperService, private geolocationService: GeolocationService, public searchService: SearchService) {
 
         this.authService.authUserObservable.subscribe(jwt => {
             if (jwt) {
@@ -26,14 +39,50 @@ export class HomePage implements OnInit {
                 this.user = null;
             }
         });
-
+/*        this.updateLocation();
+        Observable.interval(5 * 60 * 1000).subscribe(() => {
+            this.updateLocation();
+        });*/
+        this.minRating = this.initialMin / 10;
+        this.maxRating = this.initialMax / 10;
     }
 
-    ngOnInit() {
+    updateLocation() {
+        navigator.geolocation.getCurrentPosition((location) => {
+            this.geolocationService.updateLocation(location);
+        });
     }
+
 
     logout() {
         this.authService.logout();
+    }
+
+    setDualBadge(search_rating) {
+        this.minRating = search_rating.lower / 10;
+        this.maxRating = search_rating.upper / 10;
+    }
+
+    searchForPlayers() {
+        this.searchService.getSearchedPlayers(this.minRating, this.maxRating, this.maxRange);
+        this.searchListView = true;
+    }
+
+    backToSearchPanel() {
+        this.searchListView = false;
+    }
+
+    foundPlayerDetails(user: UserSearchDTO) {
+        this.detailedUser = user;
+        this.searchDetails = true;
+    }
+
+    segmentChanged(ev: any) {
+        this.detailsSwitch = ev.detail.value;
+    }
+
+    goBackToResultList() {
+        this.searchDetails = false;
     }
 
 }
