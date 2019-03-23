63d5c5db8778e8759391204ffda2cae99cfac44e
trzaskom <m_trzaskowski@wp.pl>
2019-03-23 15:12:26 +0100

    Add DTO objects interfaces

---
+++ src/client/model/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number;
+    age: number;
+    email: string;
+    gender: string;
+    name: string;
+    password: string;
+    rating: number;
+    username: string;
+    surname: string;
+}
