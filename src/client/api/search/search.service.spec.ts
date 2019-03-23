2b46ef1613176fab58e197463b8d943930e92624
trzaskom <m_trzaskowski@wp.pl>
2019-03-23 15:24:59 +0100

    Add service for getting searched players

--- src/client/api/search/search.service.spec.ts
+++ src/client/api/search/search.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { RestService } from './rest.service';
+import { SearchService } from './search.service';
 
-describe('RestService', () => {
+describe('SearchService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: RestService = TestBed.get(RestService);
+    const service: SearchService = TestBed.get(SearchService);
     expect(service).toBeTruthy();
   });
 });
---
+++ src/client/api/search/search.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {UserSearchDTO} from '../../model/UserSearchDTO';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SearchService {
+
+    searchedPlayers: UserSearchDTO[];
+
+    constructor(public http: HttpClient) {
+    }
+
+    getSearchedPlayers(minRating: string, maxRating: string, maxRange: string) {
+
+        const params = new HttpParams().set('minRating', minRating).set('maxRating', maxRating).set('maxRange', maxRange);
+
+        return this.http.get(`${environment.serverURL}/search`, {params: params}).subscribe
+        (
+            response => {
+                this.searchedPlayers = <UserSearchDTO[]>response;
+                console.log(this.searchedPlayers);
+            }
+        );
+    }
+
+}
