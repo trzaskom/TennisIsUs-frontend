f18452edef5d0134e82a78e2f7cb9723e3ac811d
trzaskom <m_trzaskowski@wp.pl>
2019-03-24 16:12:59 +0100

    Add map to details with marked locations of logged user and user from search result

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {AuthService} from '../auth/auth.service';
 import {JwtHelperService} from '@auth0/angular-jwt';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../../environments/environment';
-import {Observable} from 'rxjs/Rx';
 import {GeolocationService} from '../../client/api/geolocation/geolocation.service';
 import {SearchService} from '../../client/api/search/search.service';
 import {UserSearchDTO} from '../../client/model/UserSearchDTO';
+import {User} from '../../client/model/User';
+
+declare var google;
+let map: any;
+let infoWindow: any;
 
 @Component({
     selector: 'app-home',
@@ -14,12 +16,14 @@ import {UserSearchDTO} from '../../client/model/UserSearchDTO';
     styleUrls: ['home.page.scss']
 })
 export class HomePage {
+    @ViewChild('map') mapElement: ElementRef;
 
     searchListView = false;
     searchDetails = false;
     detailsSwitch = 'details';
     detailedUser: UserSearchDTO;
     user: string;
+    loggedUsersDetails: User;
     message: string;
     searchRating: any;
     initialMin = 10;
@@ -39,12 +43,47 @@ export class HomePage {
                 this.user = null;
             }
         });
-/*        this.updateLocation();
-        Observable.interval(5 * 60 * 1000).subscribe(() => {
-            this.updateLocation();
-        });*/
+        this.authService.getCurrentUser().subscribe(() => {
+                this.loggedUsersDetails = this.authService.whoAmI();
+            }
+        );
+        /*        this.updateLocation();
+                Observable.interval(5 * 60 * 1000).subscribe(() => {
+                    this.updateLocation();
+                });*/
         this.minRating = this.initialMin / 10;
         this.maxRating = this.initialMax / 10;
+    }
+
+    initMap() {
+        navigator.geolocation.getCurrentPosition((location) => {
+            console.log(location);
+            map = new google.maps.Map(this.mapElement.nativeElement, {
+                center: {lat: location.coords.latitude, lng: location.coords.longitude},
+                zoom: 15,
+            });
+            this.addMarker(location.coords.latitude, location.coords.longitude, 'Your current position');
+            this.addMarker(this.detailedUser.searchedLatitude, this.detailedUser.searchedLongitude,
+                this.detailedUser.name + ' ' + this.detailedUser.surname);
+            this.mapElement.nativeElement.hidden = true;
+        });
+    }
+
+    addMarker(latitude, longitude, content) {
+
+        console.log(latitude);
+        console.log(longitude);
+        const marker = new google.maps.Marker({
+            map: map,
+            animation: google.maps.Animation.DROP,
+            position: {lat: latitude, lng: longitude}
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow = new google.maps.InfoWindow();
+            infoWindow.setContent(content);
+            infoWindow.open(map, this);
+        });
     }
 
     updateLocation() {
@@ -71,10 +110,17 @@ export class HomePage {
     foundPlayerDetails(user: UserSearchDTO) {
         this.detailedUser = user;
         this.searchDetails = true;
+        this.initMap();
     }
 
     segmentChanged(ev: any) {
         this.detailsSwitch = ev.detail.value;
+        if (this.detailsSwitch === 'map') {
+            this.mapElement.nativeElement.hidden = false;
+        } else {
+            this.mapElement.nativeElement.hidden = true;
+
+        }
     }
 
     goBackToResultList() {
