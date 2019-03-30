548293693aeb3b4bb01e4fc3423b0732d07ea647
trzaskom <m_trzaskowski@wp.pl>
2019-03-30 14:15:40 +0100

    Add ts-ignore annotation for temporary unexplainable import error

--- src/client/api/search/search.service.ts
+++ src/client/api/search/search.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
+// @ts-ignore
 import {UserSearchDTO} from '../../model/UserSearchDTO';
 
 @Injectable({
