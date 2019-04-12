eab2c17b24c8d2e3c02c3a1ba1f54595f2ca64b5
trzaskom <m_trzaskowski@wp.pl>
2019-04-12 13:59:34 +0200

    Add dynamic friend list updating

--- src/app/messages/messages.page.ts
+++ src/app/messages/messages.page.ts
@@ -1,13 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import * as SockJS from 'sockjs-client';
-import * as Stomp from 'stompjs';
-import {Message} from '../../client/model/Message';
-import {environment} from '../../environments/environment';
-import {AuthService} from '../auth/auth.service';
 import {User} from '../../client/model/User';
-// @ts-ignore
-import {UserSearchDTO} from '../../client/model/UserSearchDTO';
-import {MessagesService} from './messages.service';
+import {NavController} from '@ionic/angular';
+import {FriendsService} from '../../client/api/friends/friends.service';
+import {SocketService} from '../../client/socket/socket.service';
 
 
 @Component({
@@ -16,67 +11,26 @@ import {MessagesService} from './messages.service';
     styleUrls: ['./messages.page.scss'],
 })
 export class MessagesPage implements OnInit {
-    serverUrl = environment.serverURL + '/socket';
-    isLoaded = false;
-    isCustomSocketOpened = false;
-    stompClient;
-    messages: Message[] = [];
-    loggedUsersDetails: User;
-    friends: UserSearchDTO[];
-    privateChatFlag = false;
-    privateChatReceiver: UserSearchDTO;
 
-    constructor(private readonly authService: AuthService,
-                private readonly messagesService: MessagesService) {
-        this.authService.getCurrentUser().subscribe(() => {
-                this.loggedUsersDetails = this.authService.whoAmI();
-            }
-        );
-        this.friends = this.messagesService.friends;
-    }
+    friends: User[];
 
-    ngOnInit() {
-        this.initializeWebSocketConnection();
-    }
+    constructor(private readonly socketService: SocketService, private readonly navCtrl: NavController,
+                private readonly friendsService: FriendsService) {
+        this.friendsService.getUserFriends().subscribe(_ => {
+            this.friends = this.friendsService.friends;
+        });
 
-    initializeWebSocketConnection() {
-        const ws = new SockJS(this.serverUrl);
-        this.stompClient = Stomp.over(ws);
-        const that = this;
-        this.stompClient.connect({}, function () {
-            that.isLoaded = true;
-            that.openSocket();
+        this.friendsService.newFriendObservable.subscribe(_ => {
+            this.friends = this.friendsService.friends;
         });
     }
 
-    sendMessageUsingSocket(messageContent: string) {
-        const message: Message = {
-            message: messageContent, fromId: String(this.loggedUsersDetails.id),
-            toId: String(this.privateChatReceiver.id)
-        };
-        this.stompClient.send('/socket-subscriber/send/message', {}, JSON.stringify(message));
-
-    }
-
-    openSocket() {
-        if (this.isLoaded) {
-            this.isCustomSocketOpened = true;
-            this.stompClient.subscribe('/socket-publisher/' + this.loggedUsersDetails.id, (message) => {
-                this.handleResult(message);
-            });
-        }
-    }
-
-    handleResult(message) {
-        if (message.body) {
-            const messageResult: Message = JSON.parse(message.body);
-            console.log(messageResult);
-            this.messages.push(messageResult);
-        }
+    ngOnInit() {
     }
 
     privateChat(friend) {
-        this.privateChatReceiver = friend;
-        this.privateChatFlag = true;
+        this.socketService.chosenFriend = friend;
+        this.navCtrl.navigateRoot('chat-room');
     }
+
 }
