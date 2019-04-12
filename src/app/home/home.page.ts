a4c456fbfbbdc28e9d41dbd06f42472b9e352ff8
trzaskom <m_trzaskowski@wp.pl>
2019-04-12 14:08:18 +0200

    Update and add methods for handling values from new search criteria

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,8 +6,8 @@ import {SearchService} from '../../client/api/search/search.service';
 // @ts-ignore
 import {UserSearchDTO} from '../../client/model/UserSearchDTO';
 import {User} from '../../client/model/User';
-import {Observable} from 'rxjs/Rx';
-import {MessagesService} from '../messages/messages.service';
+import {FriendsService} from '../../client/api/friends/friends.service';
+import {SocketService} from '../../client/socket/socket.service';
 
 declare var google;
 let map: any;
@@ -32,15 +32,17 @@ export class HomePage {
     loggedUsersDetails: User;
     message: string;
     searchRating: any;
-    initialMin = 10;
-    initialMax = 80;
-    minRating: any;
-    maxRating: any;
-    maxRange: any;
+    searchAge: any;
+    searchGender = 3;
+    minRating = 1;
+    maxRating = 1;
+    minAge = 16;
+    maxAge = 16;
+    maxRange = 1;
 
     constructor(private readonly authService: AuthService,
                 jwtHelper: JwtHelperService, private geolocationService: GeolocationService, public searchService: SearchService,
-                public messagesService: MessagesService) {
+                private readonly friendsService: FriendsService, private readonly socketService: SocketService) {
 
         this.authService.authUserObservable.subscribe(jwt => {
             if (jwt) {
@@ -50,49 +52,45 @@ export class HomePage {
                 this.user = null;
             }
         });
+        this.checkSocketConnection();
         this.authService.getCurrentUser().subscribe(() => {
                 this.loggedUsersDetails = this.authService.whoAmI();
             }
         );
         navigator.geolocation.watchPosition(changedLocation => this.geolocationService.updateLocation(changedLocation));
-
-        this.minRating = this.initialMin / 10;
-        this.maxRating = this.initialMax / 10;
     }
 
     initMap() {
+        map = new google.maps.Map(this.mapElement.nativeElement, {
+            center: {lat: this.geolocationService.location.latitude, lng: this.geolocationService.location.longitude},
+        });
         const currentLocIconURL = 'http://maps.google.com/mapfiles/ms/micons/blue-dot.png';
         const searchedPlayerIconURL = 'http://maps.google.com/mapfiles/ms/micons/golfer.png';
-        navigator.geolocation.getCurrentPosition((location) => {
-            console.log(location);
-            map = new google.maps.Map(this.mapElement.nativeElement, {
-                center: {lat: location.coords.latitude, lng: location.coords.longitude},
-            });
+        this.addMarker(this.geolocationService.location.latitude, this.geolocationService.location.longitude,
+            'Twoja aktualna pozycja', currentLocIconURL);
+        this.addMarker(this.detailedUser.searchedLatitude, this.detailedUser.searchedLongitude,
+            this.detailedUser.name + ' ' + this.detailedUser.surname, searchedPlayerIconURL);
 
-            this.addMarker(location.coords.latitude, location.coords.longitude, 'Your current position', currentLocIconURL);
-            this.addMarker(this.detailedUser.searchedLatitude, this.detailedUser.searchedLongitude,
-                this.detailedUser.name + ' ' + this.detailedUser.surname, searchedPlayerIconURL);
+        bounds = new google.maps.LatLngBounds();
+        bounds.extend(new google.maps.LatLng(this.geolocationService.location.latitude, this.geolocationService.location.longitude));
+        bounds.extend(new google.maps.LatLng(this.detailedUser.searchedLatitude, this.detailedUser.searchedLongitude));
 
-            bounds = new google.maps.LatLngBounds();
-            bounds.extend(new google.maps.LatLng(location.coords.latitude, location.coords.longitude));
-            bounds.extend(new google.maps.LatLng(this.detailedUser.searchedLatitude, this.detailedUser.searchedLongitude));
+        /*            directionsService = new google.maps.DirectionsService;
+                    directionsDisplay = new google.maps.DirectionsRenderer({
+                        suppressMarkers: true
+                    });
+                    directionsDisplay.setMap(map);
+                    directionsService.route({
+                        origin: {lat: location.coords.latitude, lng: location.coords.longitude},
+                        destination: {lat: this.detailedUser.searchedLatitude, lng: this.detailedUser.searchedLongitude},
+                        travelMode: 'DRIVING'
+                    }, (response, status) => {
+                        if (status === 'OK') {
+                            directionsDisplay.setDirections(response);
+                        }
+                    });*/
+        this.mapElement.nativeElement.hidden = true;
 
-            /*            directionsService = new google.maps.DirectionsService;
-                        directionsDisplay = new google.maps.DirectionsRenderer({
-                            suppressMarkers: true
-                        });
-                        directionsDisplay.setMap(map);
-                        directionsService.route({
-                            origin: {lat: location.coords.latitude, lng: location.coords.longitude},
-                            destination: {lat: this.detailedUser.searchedLatitude, lng: this.detailedUser.searchedLongitude},
-                            travelMode: 'DRIVING'
-                        }, (response, status) => {
-                            if (status === 'OK') {
-                                directionsDisplay.setDirections(response);
-                            }
-                        });*/
-            this.mapElement.nativeElement.hidden = true;
-        });
     }
 
     addMarker(latitude, longitude, content, iconURL) {
@@ -100,13 +98,9 @@ export class HomePage {
         const marker = new google.maps.Marker({
             map: map,
             icon: new google.maps.MarkerImage(
-                // URL
                 iconURL,
-                // (width,height)
                 new google.maps.Size(44, 32),
-                // The origin point (x,y)
                 new google.maps.Point(0, 0),
-                // The anchor point (x,y)
                 new google.maps.Point(22, 32)
             ),
             animation: google.maps.Animation.DROP,
@@ -120,13 +114,19 @@ export class HomePage {
         });
     }
 
-    setDualBadge(search_rating) {
+    setDualBadgeRating(search_rating) {
         this.minRating = search_rating.lower / 10;
         this.maxRating = search_rating.upper / 10;
     }
 
+    setDualBadgeAge(search_age) {
+        this.minAge = search_age.lower;
+        this.maxAge = search_age.upper;
+    }
+
     searchForPlayers() {
-        this.searchService.getSearchedPlayers(this.minRating, this.maxRating, this.maxRange);
+        this.searchService.getSearchedPlayers(String(this.maxRange), String(this.minRating), String(this.maxRating),
+            String(this.minAge), String(this.maxAge), String(this.searchGender));
         this.searchListView = true;
     }
 
@@ -137,7 +137,12 @@ export class HomePage {
     foundPlayerDetails(user: UserSearchDTO) {
         this.detailedUser = user;
         this.searchDetails = true;
-        this.initMap();
+
+        setTimeout(() => {
+            this.initMap();
+        }, 50);
+
+
     }
 
     segmentChanged(ev: any) {
@@ -157,6 +162,12 @@ export class HomePage {
     }
 
     addToFriends() {
-        this.messagesService.addToFriends(this.detailedUser);
+        this.friendsService.addToFriends(this.detailedUser.id);
+    }
+
+    checkSocketConnection() {
+        if (this.socketService.stompClient == null) {
+            this.socketService.initializeWebSocketConnection();
+        }
     }
 }
