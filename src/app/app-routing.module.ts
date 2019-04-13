265ab94db0ff60f31aa742cedf2636344c444545
trzaskom <m_trzaskowski@wp.pl>
2019-04-13 23:45:02 +0200

    Update routing paths with removing old and adding new ones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,17 @@ import {AuthGuard} from './auth/auth.guard';
 
 const routes: Routes = [
     {path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [AuthGuard]},
-    {path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthGuard]},
     {path: 'courts', loadChildren: './courts/courts.module#CourtsPageModule', canActivate: [AuthGuard]},
-    {path: 'matches', loadChildren: './matches/matches.module#MatchesPageModule', canActivate: [AuthGuard]},
     {path: 'weather', loadChildren: './weather/weather.module#WeatherPageModule', canActivate: [AuthGuard]},
     {path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule', canActivate: [AuthGuard]},
     {path: 'signup', loadChildren: './signup/signup.module#SignupPageModule'},
     {path: 'login', loadChildren: './login/login.module#LoginPageModule'},
     {path: 'rating', loadChildren: './rating/rating.module#RatingPageModule', canActivate: [AuthGuard]},
     {path: 'messages', loadChildren: './messages/messages.module#MessagesPageModule', canActivate: [AuthGuard]},
-  { path: 'chat-room', loadChildren: './messages/chat-room/chat-room.module#ChatRoomPageModule', canActivate: [AuthGuard]}
+    {path: 'chat-room', loadChildren: './chat-room/chat-room.module#ChatRoomPageModule', canActivate: [AuthGuard]},
+    {path: 'search-results', loadChildren: './search-results/search-results.module#SearchResultsPageModule', canActivate: [AuthGuard]},
+    {path: 'search-details', loadChildren: './search-details/search-details.module#SearchDetailsPageModule', canActivate: [AuthGuard]},
+    {path: 'search-form', loadChildren: './search-form/search-form.module#SearchFormPageModule', canActivate: [AuthGuard]},
 ];
 
 @NgModule({
